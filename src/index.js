38ad21f07eca21456fc895ee1a1b4b83b257f9c6
Phayouderson <108438743+Phayouderson@users.noreply.github.com>
2024-03-30 12:10:47 -0400

    update package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
-import './index.css';
 import App from './containers/App';
 import 'tachyons';
 // import registerServiceWorker from './registerServiceWorker';
